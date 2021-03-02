11b15db96b1973eb677024396e8c77d0b2e04b1d
Korty (Lily) <admin@kortyboi.com>
2021-03-02 13:18:11 -0500

    Update Main.cs
    Adapted for new Start Late functions

--- Main.cs
+++ Main.cs
@@ -11,7 +11,7 @@ namespace FrameFocus
         public const string Name = "FrameFocus"; // Name of the Mod.  (MUST BE SET)
         public const string Author = "Korty"; // Author of the Mod.  (Set as null if none)
         public const string Company = null; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "1.1.0"; // Version of the Mod.  (MUST BE SET)
+        public const string Version = "1.1.1"; // Version of the Mod.  (MUST BE SET)
         public const string DownloadLink = "https://github.com/KortyBoi/FrameFocus"; // Download Link for the Mod.  (Set as null if none)
         public const string Description = "FrameFocus is a computer performance enhancement game utility. This will allow you to gain a slight bit of performance while you are tabbed out of the game. When you are tabbed out, your frame rate will drop (this is to unfocusing the game, thus causing extra performance through out your computer). Once you tab back in, your frame rate will go back to normal. This mod also doubles as a frame rate unlocker of sorts. You can set a max target frame rate you would like the game to try and run at.";
     }
@@ -19,11 +19,11 @@ namespace FrameFocus
     public class FrameFocus : MelonMod
     {
         public static bool isDebug;
-        private static MelonPreferences_Category melon;
-        private static MelonPreferences_Entry<int> FrameLimit;
-        private static MelonPreferences_Entry<bool> allowFrameLimit;
-        private static MelonPreferences_Entry<bool> allowVRUse;
-        private static MelonPreferences_Entry<bool> override_emmVRC;
+        public static MelonPreferences_Category melon;
+        public static MelonPreferences_Entry<int> FrameLimit;
+        public static MelonPreferences_Entry<bool> allowFrameLimit;
+        public static MelonPreferences_Entry<bool> allowVRUse;
+        public static MelonPreferences_Entry<bool> override_emmVRC;
 
         public override void OnApplicationStart() // Runs after Game Initialization.
         {
@@ -32,39 +32,26 @@ namespace FrameFocus
                 isDebug = true;
                 MelonLogger.Msg("Debug mode is active");
             }
-
-           melon = MelonPreferences.CreateCategory(BuildInfo.Name, BuildInfo.Name);
+            
+            melon = MelonPreferences.CreateCategory(BuildInfo.Name, BuildInfo.Name);
             allowFrameLimit = (MelonPreferences_Entry<bool>)melon.CreateEntry("allowFrameLimit", false, "Toggle Frame Focus");
             FrameLimit = (MelonPreferences_Entry<int>)melon.CreateEntry("FrameLimit", 90, "Max Frame Limit");
             allowVRUse = (MelonPreferences_Entry<bool>)melon.CreateEntry("allowVRUse", false, "Allow VR to Limit FrameRate");
             override_emmVRC = (MelonPreferences_Entry<bool>)melon.CreateEntry("override_emmVRC", false, "Make FrameFocus ignore emmVRC integration (only works if emmVRC is detected)");
 
-            ModCompatibility.RunCompatibilityCheck();
+            //ModCompatibility.RunCompatibilityCheck();
             MelonLogger.Msg("Initialized!");
+        }
+
+        public override void VRChat_OnUiManagerInit()
+        {
+            MelonCoroutines.Start(ModCompatibility.RunCompatibilityCheck());
+            MelonCoroutines.Start(StartLate.Init());
         }
 
         public override void OnUpdate()
         {
-            if (!allowFrameLimit.Value) // false
-                return; // ends method
-            else // true
-            {
-                if (XRDevice.isPresent && !allowVRUse.Value)
-                    return;
-                if (Application.isFocused) // isFocused = If you are controlling the game a.k.a. if your mouse has been pressed onto the game
-                {
-                    if (override_emmVRC.Value && ModCompatibility.emmVRC) // ignored emmVRC integration
-                        Application.targetFrameRate = (int)FrameLimit.Value; // Sets game's FrameRate to Given Target FPS
-                    else if (ModCompatibility.emmVRC)
-                        Application.targetFrameRate = (int)GETemmVRCconfig.ReadConfig().FPSLimit;
-                    else if (ModCompatibility.emmVRC && GETemmVRCconfig.ReadConfig().UnlimitedFPSEnabled)
-                        Application.targetFrameRate = (int)300;
-                    else
-                        Application.targetFrameRate = (int)FrameLimit.Value; // Sets game's FrameRate to Given Target FPS
-                }
-                else
-                    Application.targetFrameRate = (int)5; // Sets game's FrameRate to 5 FPS
-            }
+            StartLate.OnUpdate();
         }
 
         public override void OnPreferencesSaved() 
