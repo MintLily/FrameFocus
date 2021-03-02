ceec5478659fc6ed6fc797bd7c48f5c71b8e49c4
Korty (Lily) <admin@kortyboi.com>
2021-03-02 13:19:07 -0500

    Update ModCompatibility.cs
    added a delay for the check to reduce errors
    added a try catch to let others know of the harmless error that may occur if it does

--- Utilities/ModCompatibility.cs
+++ Utilities/ModCompatibility.cs
@@ -1,4 +1,6 @@
 ﻿using MelonLoader;
+using System.Collections;
+using UnityEngine;
 
 namespace FrameFocus.Utilities
 {
@@ -6,16 +8,23 @@ namespace FrameFocus.Utilities
     {
         public static bool emmVRC;
 
-        public static void RunCompatibilityCheck()
+        public static IEnumerator RunCompatibilityCheck()
         {
-            if ((MelonHandler.Mods.FindIndex((MelonMod i) => i.Info.Name == "emmVRCLoader") != -1) || 
-                MelonHandler.Mods.FindIndex((MelonMod i) => i.Info.Name == "emmVRC") != -1)
+            yield return new WaitForSeconds(5);
+            try
             {
-                emmVRC = true;
-                if (FrameFocus.isDebug)
-                    MelonLogger.Msg("[Debug] Detected emmVRC, I will be using emmVRC's config for values.");
-                GETemmVRCconfig.LoadConfig();
+                if ((MelonHandler.Mods.FindIndex((MelonMod i) => i.Info.Name == "emmVRCLoader") != -1) ||
+                MelonHandler.Mods.FindIndex((MelonMod i) => i.Info.Name == "emmVRC") != -1)
+                {
+                    emmVRC = true;
+                    if (FrameFocus.isDebug)
+                        MelonLogger.Msg("[Debug] Detected emmVRC, I will be using emmVRC's config for values.");
+                    GETemmVRCconfig.LoadConfig();
+                }
             }
+            catch { MelonLogger.Msg("[Harmless] Could not find emmVRC Config file."); }
+
+            yield break;
         }
     }
 }
