423a07b6a4fa4ace91439b935d13f692063bd84f
Korty (Lily) <admin@kortyboi.com>
2021-05-30 18:21:20 -0400

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -9,10 +9,10 @@ namespace FrameFocus
     public static class BuildInfo
     {
         public const string Name = "FrameFocus";
-        public const string Author = "Korty (Lily)";
+        public const string Author = "Lily";
         public const string Company = null;
         public const string Version = "1.4.0";
-        public const string DownloadLink = "https://github.com/KortyBoi/FrameFocus";
+        public const string DownloadLink = "https://github.com/MintLily/FrameFocus";
         public const string Description = "FrameFocus is a computer performance enhancement game utility. This will allow you to gain a slight bit of performance while you are tabbed out of the game. When you are tabbed out, your frame rate will drop (this is to unfocusing the game, thus causing extra performance through out your computer). Once you tab back in, your frame rate will go back to normal. This mod also doubles as a frame rate unlocker of sorts. You can set a max target frame rate you would like the game to try and run at.";
     }
 
